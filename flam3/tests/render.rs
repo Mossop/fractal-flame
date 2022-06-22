d6caa73854127e8ccd966abd0569db7fc78ac041
Dave Townsend <dtownsend@oxymoronical.com>
2022-06-22 06:14:46 +0100

    Some test reorganisation

--- flam3/tests/render.rs
+++ flam3/tests/render.rs
@@ -1,6 +1,6 @@
 // These tests only pass with the libm feature enabled.
 #[cfg(feature = "libm")]
-mod integration {
+mod render {
     use std::{fs::File, io::BufReader, path::PathBuf};
 
     use flam3::{flam3_from_reader, render, RenderOptions};
@@ -13,6 +13,7 @@ mod integration {
     fn do_test(name: &str) {
         let mut test_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         test_dir.push("tests");
+        test_dir.push("render");
 
         println!("Testing {}", name);
 
@@ -50,117 +51,115 @@ mod integration {
 
     macro_rules! render_test {
         ($name:ident) => {
-            mod $name {
-                #[test]
-                fn render_test() {
-                    super::do_test(stringify!($name));
-                }
+            #[test]
+            fn $name() {
+                do_test(stringify!($name));
             }
         };
     }
 
-    render_test!(test000);
-    render_test!(test001);
-    render_test!(test002);
-    render_test!(test003);
-    render_test!(test004);
-    render_test!(test005);
-    render_test!(test006);
-    render_test!(test007);
-    render_test!(test008);
-    render_test!(test009);
-    render_test!(test010);
-    render_test!(test011);
-    render_test!(test012);
-    render_test!(test013);
-    render_test!(test014);
-    render_test!(test015);
-    render_test!(test016);
-    render_test!(test017);
-    render_test!(test018);
-    render_test!(test019);
-    render_test!(test020);
-    render_test!(test021);
-    render_test!(test022);
-    render_test!(test023);
-    render_test!(test024);
-    render_test!(test025);
-    render_test!(test026);
-    render_test!(test027);
-    render_test!(test028);
-    render_test!(test029);
-    render_test!(test030);
-    render_test!(test031);
-    render_test!(test032);
-    render_test!(test033);
-    render_test!(test034);
-    render_test!(test035);
-    render_test!(test036);
-    render_test!(test037);
-    render_test!(test038);
-    render_test!(test039);
-    render_test!(test040);
-    render_test!(test041);
-    render_test!(test042);
-    render_test!(test043);
-    render_test!(test044);
-    render_test!(test045);
-    render_test!(test046);
-    render_test!(test047);
-    render_test!(test048);
-    render_test!(test049);
-    render_test!(test050);
-    render_test!(test051);
-    render_test!(test052);
-    render_test!(test053);
-    render_test!(test054);
-    render_test!(test055);
-    render_test!(test056);
-    render_test!(test057);
-    render_test!(test058);
-    render_test!(test059);
-    render_test!(test060);
-    render_test!(test061);
-    render_test!(test062);
-    render_test!(test063);
-    render_test!(test064);
-    render_test!(test065);
-    render_test!(test066);
-    render_test!(test067);
-    render_test!(test068);
-    render_test!(test069);
-    render_test!(test070);
-    render_test!(test071);
-    render_test!(test072);
-    render_test!(test073);
-    render_test!(test074);
-    render_test!(test075);
-    render_test!(test076);
-    render_test!(test077);
-    render_test!(test078);
-    render_test!(test079);
-    render_test!(test080);
-    render_test!(test081);
-    render_test!(test082);
-    render_test!(test083);
-    render_test!(test084);
-    render_test!(test085);
-    render_test!(test086);
-    render_test!(test087);
-    render_test!(test088);
-    render_test!(test089);
-    render_test!(test090);
-    render_test!(test091);
-    render_test!(test092);
-    render_test!(test093);
-    render_test!(test094);
-    render_test!(test095);
-    render_test!(test096);
-    render_test!(test097);
-    render_test!(test098);
-    render_test!(test099);
-    render_test!(test100);
-    render_test!(test101);
-    render_test!(test102);
-    render_test!(test103);
+    render_test!(render000);
+    render_test!(render001);
+    render_test!(render002);
+    render_test!(render003);
+    render_test!(render004);
+    render_test!(render005);
+    render_test!(render006);
+    render_test!(render007);
+    render_test!(render008);
+    render_test!(render009);
+    render_test!(render010);
+    render_test!(render011);
+    render_test!(render012);
+    render_test!(render013);
+    render_test!(render014);
+    render_test!(render015);
+    render_test!(render016);
+    render_test!(render017);
+    render_test!(render018);
+    render_test!(render019);
+    render_test!(render020);
+    render_test!(render021);
+    render_test!(render022);
+    render_test!(render023);
+    render_test!(render024);
+    render_test!(render025);
+    render_test!(render026);
+    render_test!(render027);
+    render_test!(render028);
+    render_test!(render029);
+    render_test!(render030);
+    render_test!(render031);
+    render_test!(render032);
+    render_test!(render033);
+    render_test!(render034);
+    render_test!(render035);
+    render_test!(render036);
+    render_test!(render037);
+    render_test!(render038);
+    render_test!(render039);
+    render_test!(render040);
+    render_test!(render041);
+    render_test!(render042);
+    render_test!(render043);
+    render_test!(render044);
+    render_test!(render045);
+    render_test!(render046);
+    render_test!(render047);
+    render_test!(render048);
+    render_test!(render049);
+    render_test!(render050);
+    render_test!(render051);
+    render_test!(render052);
+    render_test!(render053);
+    render_test!(render054);
+    render_test!(render055);
+    render_test!(render056);
+    render_test!(render057);
+    render_test!(render058);
+    render_test!(render059);
+    render_test!(render060);
+    render_test!(render061);
+    render_test!(render062);
+    render_test!(render063);
+    render_test!(render064);
+    render_test!(render065);
+    render_test!(render066);
+    render_test!(render067);
+    render_test!(render068);
+    render_test!(render069);
+    render_test!(render070);
+    render_test!(render071);
+    render_test!(render072);
+    render_test!(render073);
+    render_test!(render074);
+    render_test!(render075);
+    render_test!(render076);
+    render_test!(render077);
+    render_test!(render078);
+    render_test!(render079);
+    render_test!(render080);
+    render_test!(render081);
+    render_test!(render082);
+    render_test!(render083);
+    render_test!(render084);
+    render_test!(render085);
+    render_test!(render086);
+    render_test!(render087);
+    render_test!(render088);
+    render_test!(render089);
+    render_test!(render090);
+    render_test!(render091);
+    render_test!(render092);
+    render_test!(render093);
+    render_test!(render094);
+    render_test!(render095);
+    render_test!(render096);
+    render_test!(render097);
+    render_test!(render098);
+    render_test!(render099);
+    render_test!(render100);
+    render_test!(render101);
+    render_test!(render102);
+    render_test!(render103);
 }
