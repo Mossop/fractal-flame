ff02cf04875da1c9cc405bebb3701cec28e1a493
Dave Townsend <dtownsend@oxymoronical.com>
2022-08-01 17:35:14 +0100

    Allow scaling to a specific target size.

--- flam3-render/src/main.rs
+++ flam3-render/src/main.rs
@@ -4,10 +4,17 @@ use std::{
     io::{stdin, BufReader},
 };
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use flam3::{flam3_from_reader, render, RenderOptions};
 use flexi_logger::Logger;
 use image::ImageFormat;
+
+#[derive(Default, Copy, Clone, ValueEnum)]
+enum SizeMode {
+    #[default]
+    Contain,
+    Crop,
+}
 
 /// A command line tool for rendering flam3 fractal flames.
 ///
@@ -15,7 +22,7 @@ use image::ImageFormat;
 /// flam3-render tool from Scott Draves. Many of the same options can be
 /// set using the same environment variables however command line argument
 /// support is also available.
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[clap(author, version, rename_all_env = "verbatim")]
 struct Args {
     /// The file to render.
@@ -38,13 +45,29 @@ struct Args {
     #[clap(long, env)]
     earlyclip: bool,
 
-    /// Magnifies the final image by this amount.
+    /// Magnifies the final image by this amount. Applies after any height/width setting.
     #[clap(long = "ss", env = "ss")]
     size_scale: Option<f64>,
 
     /// Magnifies the quality by this amount.
     #[clap(long = "qs", env = "qs")]
     quality_scale: Option<f64>,
+
+    /// Scales the image to this width.
+    #[clap(long)]
+    width: Option<u32>,
+
+    /// Scales the image to this height.
+    #[clap(long)]
+    height: Option<u32>,
+
+    /// How to scale the image when both height and width are given.
+    ///
+    /// Either the final image will contain the entire bounds of the fractal
+    /// plus some more if the aspect is different, or the fractal will be
+    /// cropped.
+    #[clap(long, value_enum, default_value = "contain")]
+    size_mode: SizeMode,
 }
 
 fn make_single_name(out: &Option<String>) -> String {
@@ -79,6 +102,38 @@ fn main() -> Result<(), Box<dyn Error>> {
     let count = genomes.len();
 
     for (index, mut genome) in genomes.into_iter().enumerate() {
+        match (args.width, args.height, args.size_mode) {
+            (Some(width), None, _) => {
+                genome.scale(width as f64 / genome.size.width as f64);
+            }
+            (None, Some(height), _) => {
+                genome.scale(height as f64 / genome.size.height as f64);
+            }
+            (Some(width), Some(height), SizeMode::Crop) => {
+                let wscale = width as f64 / genome.size.width as f64;
+                let hscale = height as f64 / genome.size.height as f64;
+                if wscale > hscale {
+                    genome.scale(wscale);
+                    genome.size.height = height;
+                } else {
+                    genome.scale(hscale);
+                    genome.size.width = width;
+                }
+            }
+            (Some(width), Some(height), SizeMode::Contain) => {
+                let wscale = width as f64 / genome.size.width as f64;
+                let hscale = height as f64 / genome.size.height as f64;
+                if wscale < hscale {
+                    genome.scale(wscale);
+                    genome.size.height = height;
+                } else {
+                    genome.scale(hscale);
+                    genome.size.width = width;
+                }
+            }
+            (None, None, _) => {}
+        }
+
         if let Some(scale) = args.size_scale {
             genome.scale(scale);
         }
